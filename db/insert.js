3a269e7a369866b9020d3bbbb769fd43b1b3feff
Rodrigo Ignacio Falco <rfalco@patagoniansys.com>
2014-11-12 12:56:09 -0300

    Player inserts

---
+++ db/insert.js
@@ -0,0 +1,3 @@
+db.players.insert({email:'rfalco@patagonian.it',fullName:'Rodrigo Falco', nick: 'Coco' });
+db.players.insert({email:'daniel.mansilla@patagonian.it',fullName:'Daniel Mansilla', nick: 'Dani' });
+db.players.insert({email:'juan.martinez@patagonian.it',fullName:'Juan Martinez', nick: 'Juan' });
--- routes/players.js
+++ routes/players.js
@@ -1,30 +1,28 @@
 var express = require('express');
-var router = express.Router();
 var debug = require('debug')('nodeFutbol');
-
 var mongo = require('mongoskin');
+
+var router = express.Router();
 var db = mongo.db("mongodb://localhost:27017/futbolNode", {native_parser:true});
 
-/* GET home page. */
+/* GET players listing. */
 router.get('/', function(req, res) {
-	debug('Listing players');
 	db.bind('players');
-	db.players.find().toArray(function(err, items) {
-		//debug('Count');
-		debug(items);
-		
-		debug('Count:' + items.length);
+  res.send('Listing players');
+  db.players.find().toArray(function(err, items) {
+  	/*
 		var players = [];
 		for (var i = 0; i < items.length; i++) {
 			debug(items[i]);
 			players[i] = items[i];
 		};
-    db.close();
-    debug('Count:' + players.length);
-    res.render('index', { title: 'Express', players: players });
+	    db.close();
+	    debug('Count:' + players.length);
+	    res.render('index', { title: 'Express', players: players });
+	    */
+	    res.render('index', { title: 'Express', players: items });
+	    db.close();
 	});
-  
-  
 });
 
 module.exports = router;
